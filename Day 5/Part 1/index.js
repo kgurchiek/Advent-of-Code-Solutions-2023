7517881aa10591954cba0d47b1c178471c794b30
Cornbread2100 <cornbread2100@cornbread2100.com>
2023-12-05 16:28:38 -0500

    Day 5 Part 1

--- Day 5/Part 1/index.js
+++ Day 5/Part 1/index.js
@@ -13,16 +13,24 @@ for (const map of maps) {
       ranges1.push({ start: parseInt(range.split(' ')[1]), end: parseInt(range.split(' ')[1]) + parseInt(range.split(' ')[2]) - 1 });
     }
     
+    const conversions = [];
     for (var i = 0; i < ranges1.length; i++) {
       const range = ranges1[i];
       const seedsKeys = Object.keys(seeds);
       for (var j = 0; j < seedsKeys.length; j++) {
+        if (conversions.includes(j)) continue;
         const seed = seeds[seedsKeys[j]];
-        if (seed >= range.start && seed <= range.end) seeds[seedsKeys[j]] = ranges2[i].start + seed - range.start;
+        if (seed >= range.start && seed <= range.end) {
+          seeds[seedsKeys[j]] = ranges2[i].start + seed - range.start;
+          conversions.push(j);
+        }
       }
     }
   }
-  console.log(seeds)
+  // console.log(seeds)
 }
 
-// console.log(seeds);
+console.log(seeds);
+var lowest;
+for (const seed in seeds) if (lowest == null || seeds[seed] < lowest) lowest = seeds[seed];
+console.log(lowest);
