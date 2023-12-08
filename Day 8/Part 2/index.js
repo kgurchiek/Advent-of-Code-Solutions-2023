db9367c46f9b6e3989a8f5450e1d23735e7c640c
kgurchiek <cornbread2100@cornbread2100.com>
2023-12-08 14:14:41 -0500

    Day 8 Part 2

--- Day 8/Part 2/index.js
+++ Day 8/Part 2/index.js
@@ -1,23 +1,41 @@
 const fs = require('fs');
 const input = fs.readFileSync('../input', 'utf8').split('\n');
 
+function greatestCommonDivisor(a, b) {  
+  if (!b) return a;  
+  return greatestCommonDivisor(b, a % b);  
+}
+
+function leastCommonMultiple(a, b) {
+  return a * b / greatestCommonDivisor(a, b)
+}
+
 const instructions = input[0].replaceAll('\r', '');
 
+const nodes = [];
 const paths = [];
-for (var i = 2; i < input.length; i++) if (input[i][2] == 'A') paths.push(i);
-console.log(paths)
+for (var i = 2; i < input.length; i++) {
+  nodes.push(input[i].substring(0, 3));
+  if (input[i][2] == 'A') paths.push(i);
+}
 
 var i = 0;
-var count = 0;
-for (; paths.filter(a => input[a][2] == 'Z').length != paths.length; count++) {
-  for (var j = 0; j < paths.length; j++) {
+const counts = [];
+for (var j = 0; j < paths.length; j++) {
+  var count = 0;
+  while (input[paths[j]][2] != 'Z') {
+    count++;
     const next = instructions[i] == 'L' ? input[paths[j]].substring(7, 10) : input[paths[j]].substring(12, 15);
-    // console.log(instructions[i], input[paths[j]])
-    for (var k = 2; k < input.length; k++) if (input[k].startsWith(next)) paths[j] = k;
+    // console.log(`${instructions[i]}, ${input[paths[j]]}`)
+    paths[j] = nodes.indexOf(next) + 2;
+    i++;
+    if (i == instructions.length) i = 0;
   }
-  i++;
-  if (i == instructions.length) i = 0;
-  // console.log(paths)
+  counts.push(count)
+  // console.log(JSON.stringify(paths))
   // console.log('')
 }
-console.log(count)
+console.log(counts)
+var total = leastCommonMultiple(counts[1], counts[0]);
+for (var i = 2; i < counts.length; i++) total = leastCommonMultiple(counts[i], total);
+console.log(total);
